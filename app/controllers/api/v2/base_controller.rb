78ecb21237af75957e09ca35a5b3a2afe2b06c5e
Joseph Magen <joseph@isratrade.co.il>
2013-10-20 12:56:47 +0300

    fixes #3018 - API v2 metadata for pagination, search, order in index collection json response

--- app/controllers/api/v2/base_controller.rb
+++ app/controllers/api/v2/base_controller.rb
@@ -9,6 +9,7 @@ module Api
       end
 
       before_filter :root_node_name, :only => :index
+      before_render :get_metadata, :only => :index
       layout 'api/v2/layouts/index_layout', :only => :index
 
       def root_node_name
@@ -19,6 +20,43 @@ module Api
                           else
                             Rabl.configuration.json_root_default_name
                           end
+      end
+
+      def get_metadata
+        @results ||= instance_variable_get("@#{controller_name}")
+        #@total should be defined in individual controllers, but in case it's not.
+        @total ||= @results.count
+        if (@search = params[:search]).present?
+          @subtotal = @results.count
+        else
+          @subtotal = @total
+        end
+
+        if params[:order].present? && (order_array = params[:order].split(' ')).any?
+          @by = order_array[0]
+          @order   = order_array[1]
+          @order ||= 'ASC'
+        end
+
+        @per_page = params[:per_page].present? ? params[:per_page].to_i : Setting::General.entries_per_page
+        @limit = if @per_page > @subtotal
+                   @subtotal
+                 else
+                   @per_page
+                 end
+
+        if params[:page].present?
+          @page = params[:page].to_i
+          @offset = (@page - 1) * @limit
+        else
+          @page = 1
+          @offset = 0
+        end
+
+        if @page * @per_page > @subtotal
+          @limit = @subtotal - (@page-1) * @per_page
+          @limit = 0 if @limit < 0
+        end
       end
 
     end
