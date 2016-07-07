fe34ae48feac7e9e168db365eec7f6b4c48dcb6d
Dominic Cleal <dominic@cleal.org>
2016-07-07 18:39:00 +0100

    Formatting

--- app/controllers/concerns/foreman/controller/parameters/nested_ancestry_common.rb
+++ app/controllers/concerns/foreman/controller/parameters/nested_ancestry_common.rb
@@ -1,6 +1,6 @@
 module Foreman::Controller::Parameters::NestedAncestryCommon
   def add_nested_ancestry_params_filter(filter)
-    filter.permit(:parent, :parent_id)
+    filter.permit :parent, :parent_id
     filter
   end
 end
--- app/controllers/concerns/foreman/controller/parameters/taxonomix.rb
+++ app/controllers/concerns/foreman/controller/parameters/taxonomix.rb
@@ -1,6 +1,7 @@
 module Foreman::Controller::Parameters::Taxonomix
   def add_taxonomix_params_filter(filter)
-    filter.permit(:locations => [], :location_ids => [], :location_names => [], :organizations => [], :organization_ids => [], :organization_names => [])
+    filter.permit :locations => [], :location_ids => [], :location_names => [],
+      :organizations => [], :organization_ids => [], :organization_names => []
     filter
   end
 end
