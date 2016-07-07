54c35f8af71ec63885c24201860f105398a0d847
Dominic Cleal <dominic@cleal.org>
2016-07-07 14:40:56 +0100

    Change filter method name to plural to match

--- app/controllers/concerns/foreman/controller/parameters/domain.rb
+++ app/controllers/concerns/foreman/controller/parameters/domain.rb
@@ -1,13 +1,13 @@
 module Foreman::Controller::Parameters::Domain
   include Foreman::Controller::Parameters::DomainParameter
 
-  def domain_param_filter
+  def domain_params_filter
     Foreman::ParameterFilters.new(::Domain).tap do |filter|
-      filter.permit(:name, :fullname, :dns_id, {:domain_parameters_attributes => [domain_parameter_param_filter]}, {}) # FIXME: opts parameter!
+      filter.permit(:name, :fullname, :dns_id, {:domain_parameters_attributes => [domain_parameter_params_filter]}, {}) # FIXME: opts parameter!
     end
   end
 
   def domain_params
-    domain_param_filter.filter_params(params, parameter_filter_context)
+    domain_params_filter.filter_params(params, parameter_filter_context)
   end
 end
--- app/controllers/concerns/foreman/controller/parameters/domain_parameter.rb
+++ app/controllers/concerns/foreman/controller/parameters/domain_parameter.rb
@@ -1,11 +1,11 @@
 module Foreman::Controller::Parameters::DomainParameter
-  def domain_parameter_param_filter
+  def domain_parameter_params_filter
     Foreman::ParameterFilters.new(::DomainParameter).tap do |filter|
       filter.permit(:id, :name, :hidden_value, :value, :_destroy, :nested, :reference_id, :nested => true)
     end
   end
 
   def domain_parameter_params
-    domain_parameter_param_filter.filter_params(params, parameter_filter_context)
+    domain_parameter_params_filter.filter_params(params, parameter_filter_context)
   end
 end
