c26982755283db9a02cc01579d1a4548760d77bd
Dominic Cleal <dominic@cleal.org>
2016-07-11 11:47:19 +0100

    Add KeepParam utility to pass unrestricted nested hashes through params
    Hosts controller tests still fail as the #hash_clone method in Host::Managed
    doesn't retain ActionController::Parameters' permitted value.

--- app/controllers/concerns/foreman/controller/parameters/nic_interface.rb
+++ app/controllers/concerns/foreman/controller/parameters/nic_interface.rb
@@ -1,14 +1,17 @@
 module Foreman::Controller::Parameters::NicInterface
+  include Foreman::Controller::Parameters::KeepParam
   include Foreman::Controller::Parameters::NicBase
 
   def nic_interface_params_filter
-    Foreman::ParameterFilter.new(::Nic::Interface).tap do |filter|
+    Foreman::ParameterFilter.new(::Nic::Interface, :top_level_hash => 'nic').tap do |filter|
       filter.permit :name, :subnet_id, :subnet, :subnet6_id, :subnet6, :domain_id, :domain
       add_nic_base_params_filter(filter)
     end
   end
 
   def nic_interface_params
-    nic_interface_params_filter.filter_params(params, parameter_filter_context)
+    keep_param(params, 'nic', :compute_attributes) do
+      nic_interface_params_filter.filter_params(params, parameter_filter_context)
+    end
   end
 end
---
+++ test/unit/concerns/parameters/nic_interface_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class NicInterfaceParametersTest < ActiveSupport::TestCase
+  include Foreman::Controller::Parameters::NicInterface
+
+  let(:context) { Foreman::ParameterFilter::Context.new(:api, 'nic', 'create') }
+
+  test "passes through :compute_attributes hash untouched" do
+    inner_params = {:name => 'test.example.com', :compute_attributes => {:foo => 'bar', :memory => 2}}
+    expects(:params).at_least_once.returns(ActionController::Parameters.new(:nic => inner_params))
+    expects(:parameter_filter_context).returns(context)
+    filtered = nic_interface_params
+
+    assert_equal 'test.example.com', filtered['name']
+    assert_equal({'foo' => 'bar', 'memory' => 2}, filtered['compute_attributes'])
+    assert filtered.permitted?
+  end
+end
