23f5223a528a49d65a1aa4b730b32c04bb416a60
Dominic Cleal <dominic@cleal.org>
2016-07-11 14:58:05 +0100

    Fix NicBase to add its attributes to the existing filter

--- app/controllers/concerns/foreman/controller/parameters/nic_base.rb
+++ app/controllers/concerns/foreman/controller/parameters/nic_base.rb
@@ -1,12 +1,10 @@
 module Foreman::Controller::Parameters::NicBase
   def add_nic_base_params_filter(filter)
-    Foreman::ParameterFilter.new(::Nic::Base).tap do |filter|
-      filter.permit :attached_to, :bond_options, # FIXME: bond_opts is array/hash?
-        :compute_attributes, :host_id, :host, :identifier, :ip, :ip6, :link, :mac,  # FIXME: compute_attributes won't mass-assign
-        :managed, :mode, :name, :password, :primary, :provider, :provision, :type,
-        :tag, :username, :virtual,
-        :_destroy, # used for nested_attributes
-        :attached_devices => []
-    end
+    filter.permit :attached_to, :bond_options, # FIXME: bond_opts is array/hash?
+      :compute_attributes, :host_id, :host, :identifier, :ip, :ip6, :link, :mac,  # FIXME: compute_attributes won't mass-assign
+      :managed, :mode, :name, :password, :primary, :provider, :provision, :type,
+      :tag, :username, :virtual,
+      :_destroy, # used for nested_attributes
+      :attached_devices => []
   end
 end
