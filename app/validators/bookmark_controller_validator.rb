b6c1936586a44304259af33abfd74029754a5c5e
Dominic Cleal <dominic@cleal.org>
2016-07-18 15:49:09 +0100

    Replace AR 'tables' with 'data_sources'
        DEPRECATION WARNING: #tables currently returns both tables and views. This behavior is deprecated and will be changed with Rails 5.1 to only return tables. Use #data_sources instead. (called from <class:BookmarkControllerValidator> at /home/dcleal/code/foreman/foreman/app/validators/bookmark_controller_validator.rb:2)
    
    Can be backported to develop.
    Not required for rails5.

--- app/validators/bookmark_controller_validator.rb
+++ app/validators/bookmark_controller_validator.rb
@@ -1,5 +1,5 @@
 class BookmarkControllerValidator < ActiveModel::EachValidator
-  @@active_record_tables = ActiveRecord::Base.connection.tables.map(&:to_s)
+  @@active_record_tables = ActiveRecord::Base.connection.data_sources.map(&:to_s)
   def validate_each(record, attribute, value)
     controllers = ["dashboard"] + (@@active_record_tables + Permission.resources.map {|x| x.tableize }).uniq
     record.errors[attribute] << _("%{value} is not a valid controller") % {:value => value } unless controllers.include?(value)
