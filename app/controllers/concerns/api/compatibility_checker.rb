48b22112083b093181e5b247516df5d92acd3ada
Dominic Cleal <dominic@cleal.org>
2016-07-19 15:13:25 +0100

    Fix Api::CompatibilityChecker for AC::Parameters#each iteration
    ActionController::Parameters#each works differently in Rails 5.0, it no
    longer yields an array of [key, value] entries for hashes, only the
    keys. This change fixes iteration for both Parameters and arrays.
    
    (Being fixed in Rails PR 27790.)
    
    Can be backported to develop.
    Required for rails5.

--- app/controllers/concerns/api/compatibility_checker.rb
+++ app/controllers/concerns/api/compatibility_checker.rb
@@ -4,9 +4,16 @@ module Api
 
     # removes unsupported "nested" flag from "host_parameters_attributes" (both Array and Hash formats supported)
     def check_create_host_nested
-      params[:host] && params[:host][:host_parameters_attributes] && params[:host][:host_parameters_attributes].each do |attribute|
-        attribute = attribute[1] if attribute.is_a? Array
-        Foreman::Deprecation.api_deprecation_warning("Field host_parameters_attributes.nested ignored") unless attribute.delete(:nested).nil?
+      if params[:host] && params[:host][:host_parameters_attributes]
+        if params[:host][:host_parameters_attributes].is_a?(Array)
+          params[:host][:host_parameters_attributes].each do |attribute|
+            Foreman::Deprecation.api_deprecation_warning("Field host_parameters_attributes.nested ignored") unless attribute.delete(:nested).nil?
+          end
+        else
+          params[:host][:host_parameters_attributes].each do |_, attribute|
+            Foreman::Deprecation.api_deprecation_warning("Field host_parameters_attributes.nested ignored") unless attribute.delete(:nested).nil?
+          end
+        end
       end
     end
   end
