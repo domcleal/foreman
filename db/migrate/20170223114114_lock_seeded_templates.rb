0c5c66b041a85e6a0bb4596edc83b4257806a5d9
Dominic Cleal <dominic@cleal.org>
2016-07-20 13:57:24 +0100

    Explicitly set DB migration compatibility to 4.2
        DEPRECATION WARNING: Directly inheriting from ActiveRecord::Migration is deprecated. Please specify the Rails release the migration was written for
    
    Put all existing DB migrations into 4.2 compatibility mode as they don't
    specify null => true on timestamp columns etc.
    
    Cannot be backported to develop.
    Not required for rails5.
    
    (cherry picked from commit 9175ba329fa8bacc23ade6ad289574e90724e582)

--- db/migrate/20170223114114_lock_seeded_templates.rb
+++ db/migrate/20170223114114_lock_seeded_templates.rb
@@ -1,7 +1,7 @@
 require Rails.root + 'db/seeds.d/07-data.rb'
 require Rails.root + 'db/seeds.d/08-data.rb'
 
-class LockSeededTemplates < ActiveRecord::Migration
+class LockSeededTemplates < ActiveRecord::Migration[4.2]
   def up
     names = (SEEDED_TEMPLATES + SEEDED_PARTITION_TABLES).map { |attrs| attrs[:name] }
     Template.where(:name => names).update_all(:locked => true)
