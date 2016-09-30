4d05ce2493ed5db55e6b167d43ab816906481e3d
Dominic Cleal <dominic@cleal.org>
2016-09-30 14:46:59 +0100

    Replace class variable with class_attribute, inherited through subclasses

--- app/models/concerns/belongs_to_proxies.rb
+++ app/models/concerns/belongs_to_proxies.rb
@@ -2,15 +2,18 @@ module BelongsToProxies
   extend ActiveSupport::Concern
 
   delegate :smart_proxies, :to => :class
+  included do
+    class_attribute :smart_proxies_from_model
+  end
 
   module ClassMethods
     def belongs_to_proxy(name, options)
       register_smart_proxy(name, options)
-      smart_proxies_from_model[name] = options
+      self.smart_proxies_from_model = (smart_proxies_from_model || {}).merge(name => options)
     end
 
     def smart_proxies
-      smart_proxies_from_model.merge(smart_proxies_from_plugins)
+      (smart_proxies_from_model || {}).merge(smart_proxies_from_plugins)
     end
 
     def register_smart_proxy(name, options)
@@ -19,10 +22,6 @@ module BelongsToProxies
     end
 
     private
-
-    def smart_proxies_from_model
-      @smart_proxies_from_model ||= {}
-    end
 
     def smart_proxies_from_plugins
       Foreman::Plugin.all.map {|plugin| plugin.smart_proxies(self) }.inject({}, :merge)
